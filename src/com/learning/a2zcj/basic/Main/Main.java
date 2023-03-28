6bf74c42ec22cfc683816311de08614c85314442
umakant <umakantdevops@gmail.com>
2023-03-28 08:04:17 +0530

    added fabonacciSeries

--- src/com/learning/a2zcj/basic/Main/Main.java
+++ src/com/learning/a2zcj/basic/Main/Main.java
@@ -1,19 +1,9 @@
 package com.learning.a2zcj.basic.Main;
 
-import com.learning.a2zcj.basics.inq.DuplicateCharacterInString;
-import com.learning.a2zcj.basics.inq.FindMaxNumber;
-
 public class Main {
 
 	public static void main(String[] args) {
 		
-		FindMaxNumber fmn=new FindMaxNumber();
-		System.out.println(fmn.getMaximuminTwoNumbers(6, 7));
-		System.out.println(fmn.getMaximuminTwoNumbers(7, 6));
-		System.out.println(fmn.getMaximuminThreeNumbers(10, 20, 30));
-		System.out.println(fmn.getMaximuminThreeNumbers(30, 20, 10));
-		System.out.println(fmn.getMaximuminThreeNumbers(20, 30, 10));
-		System.out.println(fmn.getMaximuminFourNumbers(45, 50, 20, 60));
 	}
 
 }
--- src/com/learning/a2zcj/basics/inq/FibonacciSeries.java
+++ src/com/learning/a2zcj/basics/inq/FibonacciSeries.java
@@ -2,4 +2,17 @@ package com.learning.a2zcj.basics.inq;
 
 public class FibonacciSeries {
 
+	public void printFibonacciSeries(int endpoint) {
+		int a=0,b=1;
+		int sum=0;
+		sum=a+b;
+		System.out.print(a+ " "+b);
+		while (sum<=endpoint) {
+			System.out.print(" "+sum);
+			a=b;
+			b=sum;
+			sum=a+b;
+		}
+	}
+	
 }
